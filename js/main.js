1dec347028dbd81ee2de2be7ae24f75a37bca6d3
Victoria Forte <victoria24te@gmail.com>
2018-11-12 13:11:53 -0500

    add mapbox API key

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoidnJmb3J0ZSIsImEiOiJjam9lbTcwaDkzMHpxM3BwYXR2bWhvMmVmIn0._zqgtLXFC6RJJfk1TA7wHA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
