d6a30689aaab4deff5629538c7b63b0ff823eb0e
Victoria Forte <victoria24te@gmail.com>
2018-11-16 18:12:07 -0500

    add accessibility features for logical tabbing order and screen readers

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -82,9 +82,13 @@ fetchRestaurantFromURL = (callback) => {
 fillRestaurantHTML = (restaurant = self.restaurant) => {
   const name = document.getElementById('restaurant-name');
   name.innerHTML = restaurant.name;
+  name.tabIndex = '4';
+  name.setAttribute('aria-label', restaurant.name);
 
   const address = document.getElementById('restaurant-address');
   address.innerHTML = restaurant.address;
+  address.tabIndex = '4';
+  address.setAttribute('aria-label', restaurant.address);
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
@@ -111,10 +115,14 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
 
     const day = document.createElement('td');
     day.innerHTML = key;
+    day.tabIndex = '4';
+    day.setAttribute('aria-label', key);
     row.appendChild(day);
 
     const time = document.createElement('td');
     time.innerHTML = operatingHours[key];
+    time.tabIndex = '4';
+    time.setAttribute('aria-label', operatingHours[key]);
     row.appendChild(time);
 
     hours.appendChild(row);
@@ -128,11 +136,13 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
   const title = document.createElement('h2');
   title.innerHTML = 'Reviews';
+  title.tabIndex = '5';
   container.appendChild(title);
 
   if (!reviews) {
     const noReviews = document.createElement('p');
     noReviews.innerHTML = 'No reviews yet!';
+    more.tabIndex = '5';
     container.appendChild(noReviews);
     return;
   }
@@ -150,18 +160,22 @@ createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
   name.innerHTML = review.name;
+  name.tabIndex = '5';
   li.appendChild(name);
 
   const date = document.createElement('p');
   date.innerHTML = review.date;
+  date.tabIndex = '5';
   li.appendChild(date);
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
+  rating.tabIndex = '5';
   li.appendChild(rating);
 
   const comments = document.createElement('p');
   comments.innerHTML = review.comments;
+  comments.tabIndex = '5';
   li.appendChild(comments);
 
   return li;
